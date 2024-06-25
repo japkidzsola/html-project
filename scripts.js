f51c0177ce0e603b00e577a759195281bafe14bd
japkidzsola <suzukipontzsolt@gmail.com>
2024-06-25 23:54:37 +0200

    toggle navigation

--- scripts.js
+++ scripts.js
@@ -2,3 +2,10 @@ function validateEmail(email) {
     const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     return re.test(String(email).toLowerCase());
 }
+
+ // Toggle navigation menu on small screens
+ const navToggle = document.querySelector('.nav-toggle');
+ const navMenu = document.querySelector('nav ul');
+ navToggle.addEventListener('click', function () {
+     navMenu.classList.toggle('visible');
+ });
